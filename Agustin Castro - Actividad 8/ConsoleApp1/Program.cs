6e1cec48b72de78358fab7a0ed949328e4b44f60
Agustin_Castro <agus.castro.sosa@gmail.com>
2026-04-10 12:00:08 -0300

    Add files via upload

---
+++ Agustin Castro - Actividad 8/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad_2
+{
+    class Vectores2
+    {
+        /*
+            2. Definir un vector donde almacenar los nombres de 5 países. Confeccionar el
+               algoritmo de ordenamiento alfabético.
+        */
+        private string[] vector;
+        
+        public void cargar()
+        {
+            string linea;
+            vector = new string[5];
+            for (int i = 0; i < vector.Length; i++)
+            {
+                Console.WriteLine("ingrese el nombre de un país: ");
+                linea = Console.ReadLine();
+                vector[i] = linea;
+            }
+        }
+
+         static void Main(string[] args)
+        {
+
+            Console.ReadKey();
+        }
+    }
+}
+
