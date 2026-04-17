7cba7f2f6f7c780507fdd35c7c5c4aef5ece8f22
Agustin_Castro <agus.castro.sosa@gmail.com>
2026-04-17 08:25:45 -0300

    Add files via upload

---
+++ Agustin Castro - Actividad 8/Agustin Castro - Actividad 8/Actividad 1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad_1
+{
+    class Vectores
+    {
+        /*1.
+         Cargar un vector de n elementos. imprimir el menor y un mensaje si se repite dentro
+         del vector.
+        */
+
+        private int[] Vector;
+        private int menor;
+        public void cargar()
+        {
+            Console.WriteLine("ingrese la cantidad de elementos: ");
+            string linea;
+            linea = Console.ReadLine();
+            Vector = new int[int.Parse(linea)];
+
+            for (int i = 0; i < Vector.Length; i++)
+            {
+                Console.WriteLine("ingrese un numero: ");
+                linea = Console.ReadLine();
+                Vector[i] = int.Parse(linea);
+                if(i == 0) 
+                {
+                    menor = Vector[i];
+                    Console.WriteLine("este es el numero mas chico");
+                }
+                if(menor > Vector[i])
+                {
+                    menor = Vector[i];
+                    Console.WriteLine("Ahora este es el numero mas chico");
+                }
+                if (Vector[i] == menor && i != 0)
+                {
+                    Console.WriteLine("se repite el numero menor");
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+        Vectores v1 = new Vectores();
+        v1.cargar();
+        Console.ReadKey();
+        }
+    }
+}
---
+++ Agustin Castro - Actividad 8/Agustin Castro - Actividad 8/Actividad 3/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad_3
+{
+    /*  3. Cargar un vector de n elementos de tipo entero. Ordenar posteriormente el vector
+        (el orden lo pueden elegir ustedes). */
+    class Vectores3
+    {
+        private int[] Vector;
+        private int[] VectorSinOrdenar;
+
+        public void cargar()
+        {
+            Console.WriteLine("ingrese la cantidad de elementos: ");
+            string linea;
+            linea = Console.ReadLine();
+            Vector = new int[int.Parse(linea)];
+            VectorSinOrdenar = new int[int.Parse(linea)];
+            for (int i = 0; i < Vector.Length; i++)
+            {
+                Console.WriteLine("ingrese un numero: ");
+                linea = Console.ReadLine();
+                Vector[i] = int.Parse(linea);
+                VectorSinOrdenar[i] = int.Parse(linea);
+            }
+        }
+        public void calcular()
+        {
+            int aux;
+            for (int i = 0; i < Vector.Length; i++)
+            {
+                for (int t = 0; t < Vector.Length - i - 1; t++)
+                {
+                    if (Vector[t] > Vector[t + 1])
+                    {
+                        aux = Vector[t];
+                        Vector[t] = Vector[t + 1];
+                        Vector[t + 1] = aux;
+                    }
+                }
+            }
+        }
+
+        public void mostrar()
+        {
+            Console.WriteLine("vector sin ordenar: ");
+            for (int i = 0; i<Vector.Length;i++)
+            {
+                Console.WriteLine(VectorSinOrdenar[i]);
+            }
+            Console.WriteLine("vector ordenado: ");
+            for(int i = 0; i < Vector.Length; i++)
+            {
+                Console.WriteLine(Vector[i]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Vectores3 v3 = new Vectores3();
+            v3.cargar();
+            v3.calcular();
+            v3.mostrar();
+            Console.ReadKey();
+        }
+    }
+}
