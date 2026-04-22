da53a3f3087c30ebf25f9cfac1be8141cd075f50
Agustin_Castro <agus.castro.sosa@gmail.com>
2026-04-22 00:09:56 -0300

    Add files via upload

---
+++ Agustin Castro - Act 9/Actividad 3/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad_3
+{
+    class Vectores3 
+    {
+        /*
+        3.  Se registran los nombres de 5 atletas y sus tiempos (en segundos) en una
+            carrera de 100 metros. El programa debe cargar los datos en dos vectores
+            paralelos, calcular y mostrar el promedio de los tiempos, mostrar el nombre del
+            atleta con mejor y peor tiempo, y mostrar los nombres de quienes superaron el
+            promedio
+        */
+        private string[] atletas;
+        private int[] tiempos;
+        private int promedio;
+        private string[] superaronPromedio;
+        private int contSuperaron = 0;
+        private string mejorTiempo;
+        private string peorTiempo;
+
+        public void cargar()
+        {
+            atletas = new string[5];
+            tiempos = new int[5];
+            for (int i = 0; i < atletas.Length; i++) 
+            {
+                string linea;
+                Console.WriteLine("ingrese el nombre del atleta:");
+                linea = Console.ReadLine();
+                atletas[i] = linea;
+
+                Console.WriteLine("ingrese el tiempo del atleta:");
+                linea = Console.ReadLine();
+                tiempos[i] = int.Parse(linea);
+            }
+        }
+
+        public void calcular()
+        {
+            for (int i = 0; i < tiempos.Length; i++)
+            {
+                for (int j = 0; j < tiempos.Length - i - 1; j++)
+                {
+                    if (i != 4)
+                    {
+                        if (tiempos[j] > tiempos[j + 1])
+                        {
+                            int aux;
+                            string auxAtle;
+                            aux = tiempos[j];
+                            auxAtle = atletas[j];
+                            atletas[j] = atletas[j + 1];
+                            tiempos[j] = tiempos[j + 1];
+                            atletas[j + 1] = auxAtle;
+                            tiempos[j + 1] = aux;
+                        }
+                    }
+                }
+            }
+            promedio = tiempos.Sum() / tiempos.Length;
+            mejorTiempo = atletas[0];
+            peorTiempo = atletas[4];
+            for (int i = 0; i < tiempos.Length; i++)
+            {
+                if (tiempos[i] < promedio)
+                {
+                    contSuperaron++;
+                }
+            }
+        }
+
+        public void mostrar()
+        {
+            Console.WriteLine("promedio de los tiempos: " + promedio);
+            Console.WriteLine("atleta con peor tiempo: " + peorTiempo);
+            Console.WriteLine("atleta con mejor tiempo: " + mejorTiempo);
+            Console.WriteLine("atletas con un tiempo mejor al promedio:");
+            superaronPromedio = new string[contSuperaron];
+            for (int i = 0; i < contSuperaron; i++)
+            {
+                superaronPromedio[i] = atletas[i];
+                Console.WriteLine(superaronPromedio[i] + " con un tiempo de: " + tiempos[i]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Vectores3 v3 = new Vectores3();
+            v3.cargar();
+            v3.calcular();
+            v3.mostrar();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Agustin Castro - Act 9/Actividad 4/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad_4
+{
+    class Vectores4
+    {
+        /*
+          4. Se realiza una evaluación a 6 docentes por parte de sus alumnos. Se registran
+             sus nombres y puntajes promedio obtenidos (de 1 a 10).
+             Cargar sus datos en vectores paralelos, mostrar docente con calificación más
+             alta y más baja, ordenar los vectores de mayor a menor de acuerdo con la
+             calificación y mostrar en pantalla la cantidad de docentes que aprobaron y
+             desaprobaron (tomando como base que se aprueba con una nota mayor o igual a 6)
+         */
+        private string[] docentes;
+        private int[] calificaciones;
+        private string calificacionAlta;
+        private string calificacionBaja;
+        private int aprobados = 0;
+        private int desaprobados = 0;
+
+        public void cargar()
+        {
+            docentes = new string[6];
+            calificaciones = new int[6];
+            for (int i = 0; i < docentes.Length; i++)
+            {
+                Console.WriteLine("ingrese el nombre del docente:");
+                string linea;
+                linea = Console.ReadLine();
+                docentes[i] = linea;
+                Console.WriteLine("ingrese la calificacion del docente:");
+                linea = Console.ReadLine();
+                calificaciones[i] = int.Parse(linea);
+            }
+        }
+
+        public void calcular()
+        {
+            for (int i = 0; i < calificaciones.Length; i++)
+            {
+                for (int j = 0; j < calificaciones.Length - i - 1; j++)
+                {
+                    if (i != 5)
+                    {
+                        if (calificaciones[j] > calificaciones[j + 1])
+                        {
+                            int aux;
+                            string auxDoc;
+                            aux = calificaciones[j];
+                            auxDoc = docentes[j];
+                            docentes[j] = docentes[j + 1];
+                            calificaciones[j] = calificaciones[j + 1];
+                            docentes[j + 1] = auxDoc;
+                            calificaciones[j + 1] = aux;
+                        }
+                    }
+                }
+            }
+            for (int i = 0; i < calificaciones.Length; i++)
+            {
+                if (calificaciones[i] >= 6)
+                {
+                    aprobados++;
+                }
+                else
+                {
+                    desaprobados++;
+                }
+            }
+            calificacionAlta = docentes[5];
+            calificacionBaja = docentes[0];
+        }
+
+        public void mostrar()
+        {
+            Console.WriteLine("docente con calificacion mas alta: " + calificacionAlta);
+            Console.WriteLine("docente con calificacion mas baja: " + calificacionBaja);
+            Console.WriteLine("cantidad de docentes aprobados: " + aprobados);
+            Console.WriteLine("cantidad de docentes desaprobados: " + desaprobados);
+        }
+        static void Main(string[] args)
+        {
+            Vectores4 v4 = new Vectores4();
+            v4.cargar();
+            v4.calcular();
+            v4.mostrar();
+            Console.ReadKey();
+        }
+    }
+}
