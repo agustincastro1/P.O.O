0841878db6331c4f154ae9c6f55470adf74ffc8a
Agustin_Castro <agus.castro.sosa@gmail.com>
2026-06-11 09:34:21 -0300

    Add files via upload

--- Agustin Castro - Act 16/Actividad 3/Program.cs
+++ Agustin Castro - Act 16/Actividad 3/Program.cs
@@ -17,7 +17,7 @@ namespace Actividad_3
     class Vehiculo
     {
         private string marca;
-        private float velocidadMaxima;
+        private string velocidadMaxima;
 
         public string Marca
         {
@@ -31,7 +31,7 @@ namespace Actividad_3
             }
         }
 
-        public float VelocidadMaxima
+        public string VelocidadMaxima
         {
             set
             {
@@ -46,14 +46,65 @@ namespace Actividad_3
     }
     class Auto : Vehiculo
     {
+        private string CantidadPuertas;
+        private string[] baseAuto;
+        
+        public Auto()
+        {
+            baseAuto = new string[3];
 
+            Console.WriteLine("ingrese la Marca del auto:");
+            Marca = Console.ReadLine();
+            baseAuto[0] = Marca;
+
+            Console.WriteLine("ingrese la velocidad maxima del auto (km/h):");
+            VelocidadMaxima = Console.ReadLine();
+            baseAuto[1] = VelocidadMaxima;
+
+            Console.WriteLine("ingrese la cantidad de puertas que tiene el auto");
+            CantidadPuertas = Console.ReadLine();
+            baseAuto[2] = CantidadPuertas;
+        }
+        public void mostrarDatosAuto()
+        {
+            Console.WriteLine("Datos del Auto:");
+                Console.WriteLine("Marca: " + baseAuto[0] + " Velocidad Maxima: " + baseAuto[1] + " Cantidad De Puertas: " + baseAuto[2]);
+        }
     }
 
     class Moto : Vehiculo
     {
+        private string cilindrada;
+        private string[] baseMoto;
+        public Moto()
+        {
+            baseMoto = new string[3];
+
+            Console.WriteLine("ingrese la marca de la Moto:");
+            Marca = Console.ReadLine();
+            baseMoto[0] = Marca;
+
+            Console.WriteLine("ingrese la velocidad maxima de la Moto:");
+            VelocidadMaxima = Console.ReadLine();
+            baseMoto[1] = VelocidadMaxima;
+
+            Console.WriteLine("ingrese la cilindrada de la Moto:");
+            cilindrada = Console.ReadLine();
+            baseMoto[2] = cilindrada;
+        }
+        public void mostrarDatosMoto()
+        {
+            Console.WriteLine("Datos de la Moto:");
+            Console.WriteLine("Marca: " + baseMoto[0] + " Velocidad Maxima: " + float.Parse(baseMoto[1]) + " Cilindrada: " + int.Parse(baseMoto[2]));
+        }
 
         static void Main(string[] args)
         {
+            Auto auto = new Auto();
+            Moto moto = new Moto();
+            auto.mostrarDatosAuto();
+            moto.mostrarDatosMoto();
+            Console.ReadKey();
         }
     }
 }
---
+++ Agustin Castro - Act 16/Actividad 4/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad_4
+{
+    /*
+    Armar tres clases: Animal, Mamífero y Perro.
+    La clase Animal debe tener un atributo especie.
+    La clase Mamífero, que hereda de Animal, debe tener un atributo tipoAlimentacion.
+    La clase Perro, que hereda de Mamífero, debe tener un atributo nombre.
+    Cada clase debe tener un constructor que reciba los datos correspondientes
+    y los imprima indicando a qué clase pertenecen.
+    Los datos deben ser asignados previamente
+    Crear un objeto de la clase Perro y verificar la ejecución en cadena de los constructores. 
+    */
+    class Animal
+    {
+        string Especie;
+        public string especie
+        {
+            set
+            {
+                Especie = value;
+            }
+            get
+            {
+                return Especie;
+            }
+        }
+        public Animal()
+        {
+            especie = "perro";
+            Console.WriteLine("Especie: " + especie);
+        }
+    }
+
+    class Mamifero : Animal
+    {
+        string tipoAlimentacion;
+        private  string TipoAlimentacion
+        {
+            set
+            {
+                tipoAlimentacion = value;
+            }
+            get
+            {
+                return tipoAlimentacion;
+            }
+        }
+        public Mamifero()
+        {
+            tipoAlimentacion = "Carnivoro";
+            Console.WriteLine("tipo de alimentacion: " + tipoAlimentacion);
+        }
+    }
+
+    class Perro : Mamifero
+    {
+        string Nombre;
+
+        private string nombre
+        {
+            set
+            {
+                Nombre = value;
+            }
+            get
+            {
+                return Nombre;
+            }
+        }
+        public Perro()
+        {
+            nombre = "Anda pa alla";
+            Console.WriteLine("Nombre: " +  nombre);
+        }
+        static void Main(string[] args)
+        {
+            Perro p = new Perro();
+            Console.ReadKey();
+        }
+    }
+}
