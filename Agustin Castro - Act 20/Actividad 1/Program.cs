d7291623b7d3c85df95f242e9f44ee10daf05a64
Agustin_Castro <agus.castro.sosa@gmail.com>
2026-06-25 10:53:59 -0300

    Add files via upload

--- Agustin Castro - Act 20/Actividad 1/Program.cs
+++ Agustin Castro - Act 20/Actividad 1/Program.cs
@@ -6,8 +6,57 @@ using System.Threading.Tasks;
 
 namespace Actividad_1
 {
-    internal class Program
+    /*
+Una aerolínea administra los vuelos programados mediante un sistema orientado a
+objetos.
+Plantear una clase base Vuelo que contenga como propiedades: número de vuelo,
+destino y duración en horas. Luego definir una clase derivada VueloInternacional que
+herede de Vuelo e incluya una propiedad adicional llamada paisDestino.
+Crear una lista de objetos de tipo VueloInternacional, cargar 5 vuelos con sus datos
+desde consola y luego:
+ Mostrar los vuelos registrados y el país de destino.
+ Informar cuál es el vuelo con mayor duración.
+ Mostrar el orden de ejecución de los constructores. 
+    */
+    class Vuelo
     {
+        private int NumeroVuelo;
+        private string Destino;
+        private int DuracionHoras;
+
+        public int numeroVuelo
+        {
+            set
+            {
+                NumeroVuelo = value;
+            }
+            get
+            {
+                return NumeroVuelo;
+            }
+        }
+        public string destino
+        {
+            set
+            {
+                Destino = value;
+            }
+            get
+            {
+                return Destino;
+            }
+        }
+        public int ducaionHoras
+        {
+            set
+            {
+                DuracionHoras = value;
+            }
+            get
+            {
+                return DuracionHoras;
+            }
+        }
         static void Main(string[] args)
         {
         }
