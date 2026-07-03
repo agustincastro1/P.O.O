d33e31c97999c509621c55adc7eecedf58f28bc5
Agustin_Castro <agus.castro.sosa@gmail.com>
2026-07-03 00:31:47 -0300

    Add files via upload

---
+++ Agustin Castro - Act 22/Actividad 1/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad_1
+{
+    /*
+     Crear una clase ClaseGimnasio con atributos: nombreClase, horaInicio y horaFin (usar
+DateTime).
+ Implementar un constructor que permita cargar los datos desde consola y otro
+que cargue valores por defecto (sobrecarga de constructores).
+ Incluir un método para calcular la duración de la clase usando TimeSpan.
+ Crear un vector de 3 clases de gimnasio y mostrar:
+1. La clase que tenga la mayor duración.
+2. El nombre y el horario de inicio de la clase más temprana.
+     */
+    class ClaseGimnasio
+    {
+        private string nombreClase;
+        private DateTime horaInicio;
+        private DateTime horaFin;
+
+        public string NombreClase
+        {
+            set
+            {
+                this.nombreClase = value;
+            }
+            get
+            {
+                return this.nombreClase;
+            }
+        }
+
+        public DateTime HoraInicio
+        {
+            set
+            {
+                this.horaInicio = value;
+            }
+            get
+            {
+                return this.horaInicio;
+            }
+        }
+
+        public DateTime HoraFin
+        {
+            set
+            {
+                this.horaFin = value;
+            }
+            get
+            {
+                return this.horaFin;
+            }
+        }
+
+        public ClaseGimnasio()
+        {
+            Console.WriteLine("Ingrese el nombre de la clase:");
+            this.nombreClase = Console.ReadLine();
+            Console.WriteLine("Ingrese la hora de inicio (Hora:Minutos):");
+            this.horaInicio = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la hora de fin (Hora:Minutos):");
+            this.HoraFin = DateTime.Parse(Console.ReadLine());
+        }
+
+        public ClaseGimnasio(string nombre, DateTime inicio, DateTime fin)
+        {
+            this.nombreClase = nombre;
+            this.horaInicio = inicio;
+            this.HoraFin = fin;
+        }
+
+        public TimeSpan CalcularDuracion()
+        {
+            TimeSpan duracion = this.HoraFin - this.horaInicio;
+            Console.WriteLine($"Duración de la clase {this.nombreClase}: {duracion}");
+            return duracion;
+        }
+
+
+        static void Main(string[] args)
+        {
+            ClaseGimnasio[] clases = new ClaseGimnasio[3];
+            clases[0] = new ClaseGimnasio();
+            clases[1] = new ClaseGimnasio("Yoga", new DateTime(2026, 6, 1, 8, 0, 0), new DateTime(2026, 6, 1, 9, 0, 0));
+            clases[2] = new ClaseGimnasio("Spinning", new DateTime(2026, 6, 1, 10, 0, 0), new DateTime(2026, 6, 1, 11, 0, 0));
+
+            TimeSpan duracionMayor = clases[0].CalcularDuracion();
+            ClaseGimnasio mayorDuracion = clases[0];
+
+            for (int i = 1; i < clases.Length; i++)
+            {
+                TimeSpan duracionActual = clases[i].CalcularDuracion();
+                if (duracionActual > duracionMayor)
+                {
+                    duracionMayor = duracionActual;
+                    mayorDuracion = clases[i];
+                }
+            }
+
+            Console.WriteLine($"La clase de mayor duración es: {mayorDuracion.NombreClase} ({duracionMayor})");
+
+            Console.WriteLine("--------------------------------------------------");
+
+            ClaseGimnasio masTemprana = clases[0];
+
+            for (int i = 1; i < clases.Length; i++)
+            {
+                if (clases[i].HoraInicio < masTemprana.HoraInicio)
+                {
+                    masTemprana = clases[i];
+                }
+            }
+
+            Console.WriteLine($"La clase más temprana es: {masTemprana.NombreClase} a las {masTemprana.HoraInicio:HH:mm}");
+        }
+    }
+}
